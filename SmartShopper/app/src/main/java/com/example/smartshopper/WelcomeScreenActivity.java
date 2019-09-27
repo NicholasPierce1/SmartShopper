9f4aeeb0aa9aec2af5a5e253542991d893c1c964
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2019-09-27 16:23:27 -0500

    appended controller class and amended manifest accordingly

---
+++ SmartShopper/app/src/main/java/com/example/smartshopper/WelcomeScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.smartshopper;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeScreenActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.welcome_screen);
+
+    }
+}
