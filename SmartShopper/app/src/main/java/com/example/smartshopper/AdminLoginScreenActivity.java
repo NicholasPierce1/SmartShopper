4c6479cd18708fa2c8971726b74ba12b3c9a6f30
Matthew Berry <41410700+Infraction71@users.noreply.github.com>
2019-09-29 16:50:41 -0500

    Added pw field and login intent logic

--- SmartShopper/app/src/main/java/com/example/smartshopper/AdminLoginScreenActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/AdminLoginScreenActivity.java
@@ -1,6 +1,11 @@
 package com.example.smartshopper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +17,35 @@ public class AdminLoginScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.admin_login);
 
+        Intent ini = getIntent();
+
     }
+    public void loginAction(View v){
+        EditText usernameET = findViewById(R.id.userNameET);
+        String user = usernameET.getText().toString();
+        EditText passwordET = findViewById(R.id.PasswordET);
+        String pw = passwordET.getText().toString();
+        if(validateLogin(user, pw)){
+            Intent toHub = new Intent(this, AdminScreenActivity.class);
+            toHub.putExtra("EMPID", "001");
+            startActivity(toHub);
+        }
+        else{
+            TextView result = findViewById(R.id.ResultTV);
+            result.setVisibility(View.VISIBLE);
+        }
+
+    }
+    private boolean validateLogin(String user, String pw){
+        //For now this wull be a hard codeed thing.
+        // In the Future we will have a method that validates username and password.
+        if(user.equals("001")){
+            if(pw.equals("admin")){
+                return true;
+            }
+            else return false;
+        }
+        else return false;
+    }
+
 }
