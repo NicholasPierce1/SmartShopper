d2e1e09da68775eee098150c747226977b42d736
corder1999 <42747372+corder13@users.noreply.github.com>
2019-10-01 18:08:50 -0500

    had to delete my activity and start a new name.
    I needed to change the name of the activity.

--- SmartShopper/app/src/main/java/com/example/smartshopper/SearchMainActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/SearchMainActivity.java
@@ -3,14 +3,12 @@ package com.example.smartshopper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchMainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_search_main);
     }
-
-
 }
