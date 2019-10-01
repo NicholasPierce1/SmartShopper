eda3efd3f23838239f45add68636d1cc3ddb45c2
corder1999 <42747372+corder13@users.noreply.github.com>
2019-10-01 18:14:07 -0500

    addede a few components

--- SmartShopper/app/src/main/java/com/example/smartshopper/SearchMainActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/SearchMainActivity.java
@@ -2,7 +2,9 @@ package com.example.smartshopper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SearchMainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SearchMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_main);
+    }
+
+    public void adminActionIntent(View v){
+        Intent adminIntent = new Intent(this, AdminLoginScreenActivity.class){
+            
+        }
     }
 }
