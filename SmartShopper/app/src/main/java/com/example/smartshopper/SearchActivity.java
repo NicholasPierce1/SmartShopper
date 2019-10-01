0f81ffbdb59b74f21215f66dfbb7d07e4a8ddd3a
corder1999 <42747372+corder13@users.noreply.github.com>
2019-10-01 09:35:17 -0500

    Added the Search Actvity

---
+++ SmartShopper/app/src/main/java/com/example/smartshopper/SearchActivity.java
@@ -0,0 +1,18 @@
+package com.example.smartshopper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class SearchActivity extends AppCompatActivity {
+    EditText searchET;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        searchET = findViewById(R.id.searchET);
+    }
+
+
+}
