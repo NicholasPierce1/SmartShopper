d7007f449ae7a2fda3697b471ee9f4f3f2276314
corder1999 <42747372+corder13@users.noreply.github.com>
2019-10-01 17:55:36 -0500

    changed the components
    Changed the textviews into buttons.

--- SmartShopper/app/src/main/java/com/example/smartshopper/SearchActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/SearchActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 public class SearchActivity extends AppCompatActivity {
-    EditText searchET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
