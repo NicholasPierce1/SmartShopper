0069ca8f8a6c1ebddaa4c4da66220db24b8d7ace
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2019-10-01 23:15:03 -0500

    Refactored Name of store activity
    No longer launcher
    
    open issues:
    - modidify intent filter to target "welcome_screen_activity"
    - search.xml doesn't exist. Yields build errors

--- SmartShopper/app/src/main/java/com/example/smartshopper/StoreScreenActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/StoreScreenActivity.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class WelcomeScreenActivity extends AppCompatActivity {
+public class StoreScreenActivity extends AppCompatActivity {
 
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.welcome_screen);
+        super.setContentView(R.layout.store_screen);
 
     }
 }
