fcecd7bfda33e8ebdb7661d8bb7bf922a50952e1
corder1999 <42747372+corder13@users.noreply.github.com>
2019-10-03 10:29:08 -0500

    finished the spinner

--- SmartShopper/app/src/main/java/com/example/smartshopper/welcome_screenActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/welcome_screenActivity.java
@@ -5,14 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class welcome_screenActivity extends AppCompatActivity {
+public class welcome_screenActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    String[] stores = { "Store 1"};
+    Spinner spin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.welcome_screen);
+        setContentView(R.layout.activity_main);
+        spin = findViewById(R.id.storeMenuSPNR);
+        spin.setOnItemSelectedListener(this);
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,stores);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(aa);
     }
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(), stores[position] , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+
+    }
+
     public void adminIntent(View v) {
         Intent adminIntent = new Intent(this, AdminLoginScreenActivity.class);
         startActivity(adminIntent);
@@ -22,6 +45,8 @@ public class welcome_screenActivity extends AppCompatActivity {
         Intent searchIntent = new Intent(this, StoreScreenActivity.class);
         startActivity(searchIntent);
     }
+
+
 
 
 }
