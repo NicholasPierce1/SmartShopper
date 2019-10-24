e646dc51a4073c006eea235c431b362e7a868e09
Matthew Berry <41410700+Infraction71@users.noreply.github.com>
2019-10-24 11:18:25 -0500

    added logic that creates admin and store
    using stubbed methods.

--- SmartShopper/app/src/main/java/com/example/smartshopper/Admin.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/Admin.java
@@ -7,6 +7,7 @@ public final class Admin {
     String objectID;
 
     // enumerates local properties
+    public static String[] a1 = {"Matthew Berry", "001", "admin", "Owner", "001"};
 
     public String name; // name of employee in relation
 
@@ -17,4 +18,45 @@ public final class Admin {
     public AdminLevel adminLevel; // denotes level of privileges that an admin can perform
 
     public Store store; // denotes store that admin presides in
+
+    public Admin(){
+
+    }
+
+
+    private Admin(String name, String userName, String pw, AdminLevel level, Store store){
+        this.name = name;
+        this.userName = userName;
+        password = pw;
+        adminLevel = level;
+        this.store = store;
+    }
+    public static Admin adminBuilder(String adminID){
+        String[] adminParams = getAdminDetails(adminID);
+         String nme, aid, passwrd, adlevel, sid;
+         AdminLevel aLevel;
+         Store aStore = new Store();
+        nme = adminParams[0];
+        aid = adminParams[1];
+        passwrd = adminParams[2];
+        adlevel = adminParams[3];
+        if(adlevel.equals("Admin")){
+            aLevel = AdminLevel.storeAdmin;
+        }
+        else if(adlevel.equals("Store Admin")){
+            aLevel = AdminLevel.storeAdmin;
+        }
+        else aLevel = AdminLevel.owner;
+        sid = adminParams[4];
+        aStore = Store.storeBuilder(sid);
+
+        Admin admin = new Admin(nme, aid, passwrd, aLevel, aStore);
+        return admin;
+    }
+
+    private static String[] getAdminDetails(String adminID){
+        //At some point this calls the class that class the db and retunrs an object based on adminin
+        return a1;
+    }
+
 }
--- SmartShopper/app/src/main/java/com/example/smartshopper/Store.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/Store.java
@@ -7,8 +7,32 @@ public final class Store {
     String objectID;
 
     // enumerates local properties
+    public static String[] s1 = {"Maryville,MO", "Ez Shop"};
 
     public String location;  // "city,StateAbbreviation"
 
     public String name; // type of store like "Walmart"
+
+    public Store(){
+
+    }
+    private Store(String location, String name){
+        this.location = location;
+        this.name = name;
+    }
+    public static Store storeBuilder(String storeID){
+        String[] storeParams = getStoreParams(storeID);
+        String slocation, sname;
+        slocation = storeParams[0];
+        sname = storeParams[1];
+        Store store = new Store(slocation, sname);
+        return store;
+    }
+
+    private static String[] getStoreParams(String storeID){
+        //stubbed method like the one in Admin
+        return s1;
+    }
+
+
 }
