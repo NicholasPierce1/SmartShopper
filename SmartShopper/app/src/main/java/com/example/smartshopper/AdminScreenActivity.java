c34430c2219e864a5e0bfaeb0097c6a99f46722a
Matthew Berry <41410700+Infraction71@users.noreply.github.com>
2019-10-24 17:14:48 -0500

    Creating mock data for m2

--- SmartShopper/app/src/main/java/com/example/smartshopper/AdminScreenActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/AdminScreenActivity.java
@@ -14,7 +14,7 @@ public class AdminScreenActivity extends AppCompatActivity {
 
     TextView nameTV, empIDTV, rankTV;
 
-    public static String[] a1 = {"Matthew Berry", "001", "admin", "Owner", "001"};
+
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -77,31 +77,7 @@ public class AdminScreenActivity extends AppCompatActivity {
         startActivity(logOut);
     }
 
-    public static Admin adminBuilder(String adminID){
-        String[] adminParams = getAdminDetails(adminID);
-        String nme, aid, passwrd, adlevel, sid;
-        AdminLevel aLevel;
-        Store aStore = new Store();
-        nme = adminParams[0];
-        aid = adminParams[1];
-        passwrd = adminParams[2];
-        adlevel = adminParams[3];
-        if(adlevel.equals("Admin")){
-            aLevel = AdminLevel.storeAdmin;
-        }
-        else if(adlevel.equals("Store Admin")){
-            aLevel = AdminLevel.storeAdmin;
-        }
-        else aLevel = AdminLevel.owner;
-        sid = adminParams[4];
-        aStore = Store.storeBuilder(sid);
 
-        Admin admin = new Admin(nme, aid, passwrd, aLevel, aStore);
-        return admin;
-    }
 
-    private static String[] getAdminDetails(String adminID){
-        //At some point this calls the class that class the db and retunrs an object based on adminin
-        return a1;
-    }
+
 }
