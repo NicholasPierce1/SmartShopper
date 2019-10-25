8de008d4c9390e2e335857b04b911657a8ecba0b
corder1999 <42747372+corder13@users.noreply.github.com>
2019-10-25 12:16:58 -0500

    finished the search screen rv adapter and view holder

--- SmartShopper/app/src/main/java/com/example/smartshopper/Search_Screen_RecyclerView_Adapter.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/Search_Screen_RecyclerView_Adapter.java
@@ -1,5 +1,32 @@
 package com.example.smartshopper;
 
+import android.graphics.ColorSpace;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class Search_Screen_RecyclerView_Adapter{
-    
+
+
+    public static class SearchScreenViewHolder extends RecyclerView.ViewHolder{
+        public LinearLayout linearView;
+
+        public SearchScreenViewHolder(LinearLayout v){
+            super(v);
+            linearView = v;
+        }
+    }
+
+    @NonNull
+    public Search_Screen_RecyclerView_Adapter.SearchScreenViewHolder
+    onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.search_screen_rv, parent, false);
+        SearchScreenViewHolder vh = new SearchScreenViewHolder(v);
+        return vh;
+    }
+
 }
