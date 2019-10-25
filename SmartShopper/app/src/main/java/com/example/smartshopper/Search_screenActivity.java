62958945dd6e336608f960dcf523aa8034fe873f
corder1999 <42747372+corder13@users.noreply.github.com>
2019-10-25 17:21:03 -0500

    created fake data for the recycler view

--- SmartShopper/app/src/main/java/com/example/smartshopper/Search_screenActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/Search_screenActivity.java
@@ -9,11 +9,31 @@ import android.widget.TextView;
 public class Search_screenActivity extends AppCompatActivity {
     TextView errorTV;
     EditText itemEntered;
+
+    Store store1 = new Store("Maryville", "Hyvee");
+    Store store2 = new Store("Maryville", "Walmart");
+
+    Department produce = new Department(DepartmentType.produce, true, 1, 7, store1);
+    Department grocery = new Department(DepartmentType.grocery, true, 1, 20, store2);
+
+    Commodity item1 = new Commodity("0001", "Apple", "Red Delicious", 0.99, true, Location.produceDepartmentLeftMostAisle, produce);
+    Commodity item2 = new Commodity("0002", "Orange", "Sunkist", 0.89, true, Location.produceDepartmentLeftMostAisle, produce);
+    Commodity item3 = new Commodity("0003", "Baked beans", "Bush's", 1.00, true, Location.aisleOneLeft, grocery);
+    Commodity item4 = new Commodity("0004", "24pk water", "Ice Mountain", 2.89, true, Location.aisleFourRight, grocery);
+    Commodity item5 = new Commodity("0005", "2lb sugar", "C & H", 2.49, true, Location.aisleFiveLeft, grocery);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_screen);
         errorTV = findViewById(R.id.departmentTV);
+        itemEntered = findViewById(R.id.itemNameET);
+    }
+
+    public void itemSearch(View v){
+        if(itemEntered.equals(null)){
+            errorTV.setText("");
+        }
     }
 
     public void welcomeScreenIntent(View v){
