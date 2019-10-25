f8e72ba1f09588882546cd42c575cca60c0153c0
corder1999 <42747372+corder13@users.noreply.github.com>
2019-10-25 17:49:18 -0500

    finished the itemSearch method

--- SmartShopper/app/src/main/java/com/example/smartshopper/Search_screenActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/Search_screenActivity.java
@@ -6,7 +6,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Search_screenActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Search_screenActivity extends AppCompatActivity{
+    Random rando = new Random();
     TextView errorTV;
     EditText itemEntered;
 
@@ -22,19 +26,51 @@ public class Search_screenActivity extends AppCompatActivity {
     Commodity item4 = new Commodity("0004", "24pk water", "Ice Mountain", 2.89, true, Location.aisleFourRight, grocery);
     Commodity item5 = new Commodity("0005", "2lb sugar", "C & H", 2.49, true, Location.aisleFiveLeft, grocery);
 
+    ArrayList<Object> itemListReturn0 = new ArrayList<>();
+    ArrayList<Object> itemListReturn1 = new ArrayList<>();
+    ArrayList<Object> itemListReturn2 = new ArrayList<>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_screen);
-        errorTV = findViewById(R.id.departmentTV);
+        errorTV = findViewById(R.id.errorTV);
         itemEntered = findViewById(R.id.itemNameET);
     }
 
     public void itemSearch(View v){
-        if(itemEntered.equals(null)){
-            errorTV.setText("");
+        if(itemEntered.getText().toString().isEmpty()){
+            errorTV.setText("This field cannot be blank");
+        }
+        else if(itemEntered.getText().toString().length() < 4){
+            errorTV.setText("Invalid input");
         }
     }
+
+    public ArrayList<Object> findItemByUserText(String item){
+        int number = rando.nextInt(3);
+
+        itemListReturn0.add(item1);
+        itemListReturn0.add(item2);
+        itemListReturn0.add(item3);
+        itemListReturn0.add(item4);
+        itemListReturn0.add(item5);
+
+        itemListReturn1.add(item2);
+        itemListReturn1.add(item4);
+        itemListReturn1.add(item5);
+
+        if(number == 0){
+            return itemListReturn0;
+        }
+        else if(number == 1){
+            return itemListReturn1;
+        }
+        else
+            return itemListReturn2;
+        
+    }
+
 
     public void welcomeScreenIntent(View v){
         Intent welcomeIntent = new Intent(this, Welcome_screenActivity.class);
