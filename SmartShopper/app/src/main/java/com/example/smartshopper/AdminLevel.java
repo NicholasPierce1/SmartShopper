751822e36b60fdcccb3ecc18daa44ff6c30f4330
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2019-11-04 18:03:21 -0600

    Permits admin level creation from an int

--- SmartShopper/app/src/main/java/com/example/smartshopper/AdminLevel.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/AdminLevel.java
@@ -1,5 +1,29 @@
 package com.example.smartshopper;
 
+import androidx.annotation.AnyThread;
+import androidx.annotation.Nullable;
+
 public enum AdminLevel {
-    storeAdmin, managingStoreAdmin, owner
+    storeAdmin(0), managingStoreAdmin(1), owner(2);
+
+    // holds local ordinal value in instance definition
+    private int idType;
+
+    private AdminLevel(int idType){
+        this.idType =  idType;
+    }
+
+    // facilitates creation of admin object via customer ordinal
+    @Nullable
+    @AnyThread
+    public AdminLevel getAdminLevelFromInt(int i){
+
+        // employs a switch to return acceptable variations of admin
+        switch(i){
+            case 0: return AdminLevel.storeAdmin;
+            case 1: return AdminLevel.managingStoreAdmin;
+            case 2: return AdminLevel.owner;
+            default: return null;
+        }
+    }
 }
