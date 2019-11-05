ecfca8a91e1bc3de1e5ccf8f62f84eba93184860
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2019-11-04 18:26:26 -0600

    Finishes data access root class

--- SmartShopper/app/src/main/java/com/example/smartshopper/DataAccess.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/DataAccess.java
@@ -2,24 +2,30 @@ package com.example.smartshopper;
 
 import androidx.annotation.NonNull;
 
+import com.parse.ParseObject;
+
 import java.io.Serializable;
 
 // retains shared definition and functionality employed by data access types
-abstract class DataAccess implements Serializable {
+public abstract class DataAccess implements Serializable {
 
     // defines shared, locally used objectId value (universal, surrogate key used in back4app)
     private String objectId;
 
     // default constructor for empty declaration
-    DataAccess(){}
+    public DataAccess(){}
 
     // allows for package private retrieval of object id
     @NonNull
-    String getObjectId(){
+    final String getObjectId(){
         assert(this.objectId != null);
 
         return this.objectId;
     }
 
-    
+    // assigns objectId from parse object
+    public final void setObjectIdFromParseObject(@NonNull final ParseObject parseObject){
+        this.objectId = parseObject.getObjectId();
+    }
+
 }
