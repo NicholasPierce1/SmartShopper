2642482a19552a3897615a8532b85bb8418473cd
NicholasPierce1 <42748558+NicholasPierce1@users.noreply.github.com>
2019-11-04 19:42:28 -0600

    created StoredDept to hold proxy state between stores and departments

--- SmartShopper/app/src/main/java/com/example/smartshopper/DataAccess.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/DataAccess.java
@@ -1,5 +1,8 @@
 package com.example.smartshopper;
 
+import android.content.res.Resources;
+import android.net.IpSecManager;
+
 import androidx.annotation.NonNull;
 
 import com.parse.ParseObject;
@@ -11,6 +14,9 @@ public abstract class DataAccess implements Serializable {
 
     // defines shared, locally used objectId value (universal, surrogate key used in back4app)
     private String objectId;
+
+    // determines that object was fully created before configured into parse object
+    private boolean hasBeenCreated = false;
 
     // default constructor for empty declaration
     public DataAccess(){}
@@ -27,5 +33,15 @@ public abstract class DataAccess implements Serializable {
     public final void setObjectIdFromParseObject(@NonNull final ParseObject parseObject){
         this.objectId = parseObject.getObjectId();
     }
+
+    // asserts that object has been created before conversion
+    public final void assertDataAccessObjectHasBeenCreated(){if(!this.hasBeenCreated) throw new Resources.NotFoundException("must set data access state before conversion to parse object");}
+
+    // sets hasBeenCreated after parse object creation
+    public final void setHasBeenCreated(){this.hasBeenCreated = true;}
+
+    // converts itself to a ParseObject for searching
+    @NonNull
+    public abstract ParseObject convertToParseObject(@NonNull final ParseObject parseObject);
 
 }
---
+++ SmartShopper/app/src/main/java/com/example/smartshopper/StoredDept.java
@@ -0,0 +1,74 @@
+package com.example.smartshopper;
+
+import androidx.annotation.NonNull;
+
+import com.parse.ParseObject;
+
+import java.io.Serializable;
+
+// relational object to store state between department and store relations-- proxy for creation of department objects
+final class StoredDept extends DataAccess implements Serializable {
+
+    // local string keys to create, and id cols within relation
+    private static final String hasAisleKey = "hasAisle";
+    private static final String minAisleKey = "minAisle";
+    private static final String maxAisleKey = "maxAisle";
+    private static final String departmentObjectIdKey = "departmentObjectId";
+    private static final String storeObjectIdKey = "storeObjectId";
+
+    // enumerates package private state for proxy-relation
+    boolean hasAisle; // used to determine if department retains any aisle range
+
+    // if retains aisle range, denotes threshold of range, else -1 is value
+    int minAisle;
+    int maxAisle;
+
+    // holds non-composite keys to indicate store and department
+    String departmentObjectId;
+    String storeObjectId;
+
+    // defines empty constructor for parse object creation
+    private StoredDept(){}
+
+    // creates StoredDept from dept and store as parse object w/ other state. Sets hasBeenCreated
+    static StoredDept setStoredDeptState(@NonNull final ParseObject store, @NonNull final ParseObject dept){
+        return StoredDept.setStoreDeptStateWithAisleRanges(store,dept,false, -1,-1);
+    }
+
+    // overload that passes min and max aisle range
+    static StoredDept setStoreDeptStateWithAisleRanges(@NonNull final ParseObject store, @NonNull final ParseObject dept, final boolean hasAisle, final int minAisle, final int maxAisle){
+
+        // creates local instance for state config
+        final StoredDept storedDept = new StoredDept();
+
+        // assigns store and department id
+        storedDept.departmentObjectId = dept.getObjectId();
+        storedDept.storeObjectId = dept.getObjectId();
+
+        // sets dept ranges
+        storedDept.hasAisle = hasAisle;
+        storedDept.maxAisle = hasAisle ? maxAisle: -1;
+        storedDept.minAisle = hasAisle ? minAisle: -1;
+
+        // sets that object has been created
+        storedDept.setHasBeenCreated();
+
+        return storedDept;
+    }
+
+    // overrides data access abstraction for parse object conversion granted that DA has been set first
+    @Override
+    @NonNull
+    public ParseObject convertToParseObject(@NonNull final ParseObject parseObject){
+        // asserts DA has been created beforehand
+        super.assertDataAccessObjectHasBeenCreated();
+
+        parseObject.put(StoredDept.hasAisleKey, this.hasAisle);
+        parseObject.put(StoredDept.maxAisleKey, this.maxAisle);
+        parseObject.put(StoredDept.minAisleKey, this.minAisle);
+        parseObject.put(StoredDept.departmentObjectIdKey, this.departmentObjectId);
+        parseObject.put(StoredDept.storeObjectIdKey, this.storeObjectId);
+
+        return parseObject;
+    }
+}
