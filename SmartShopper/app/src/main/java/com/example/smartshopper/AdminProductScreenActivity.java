cc3ea03d67b58c776a323635cbc5439c6e56c506
Matthew Berry <41410700+Infraction71@users.noreply.github.com>
2019-11-10 21:36:21 -0600

    Barcode enter CUD changes

--- SmartShopper/app/src/main/java/com/example/smartshopper/AdminProductScreenActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/AdminProductScreenActivity.java
@@ -3,6 +3,7 @@ package com.example.smartshopper;
 import android.content.Intent;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.view.SurfaceControl;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,38 +11,43 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 //@Author Matthew Berry
 
-public class AdminProductScreenActivity extends AppCompatActivity {
+public class AdminProductScreenActivity extends AppCompatActivity
+implements ProductCUD.CUDopperations {
+
+    ProductCUD cud;
+    ProductInput input;
     String empid, barcode, vendor, name, dept;
     double price;
     String wrong = "";
     int submitCode = -1;
-    TextView nameTV, vendorTV, deptTV, isleTV, priceTV, resultTV;
-    EditText barCodeET, nameET, vendorET, deptET, isleET, priceET;
+    TextView resultTV;
     Button crateBTN, modifyBTN, deleteBTN, pSubmitBTN, pCancleBTN, backBTN;
+    FragmentManager manager;
 
 
     @Override
-    public void onCreate(final Bundle savedInstanceState){
+    public void onCreate(final Bundle savedInstanceState) {
         final AdminProductScreenActivity controller = this;
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.admin_product);
         Intent ini = getIntent();
         empid = ini.getStringExtra("EMPID");
+        manager = getSupportFragmentManager();
+        //Creates new fragments for use
+        final FragmentTransaction transaction = manager.beginTransaction();
+        cud = new ProductCUD();
+        transaction.add( R.id.modifyCTNR, cud);
+        input = new ProductInput();
+        transaction.add(R.id.modifyCTNR, input);
+        transaction.show(cud);
+        transaction.hide(input);
+        transaction.commit();
 
-        nameTV = findViewById(R.id.NameTV);
-        vendorTV = findViewById(R.id.VendorTV);
-        deptTV = findViewById(R.id.DeptTV);
-        isleTV = findViewById(R.id.islieTV);
-        priceTV = findViewById(R.id.priceTV);
-        resultTV = findViewById(R.id.ResultTV);
-        barCodeET = findViewById(R.id.BarcodeET);
-        nameET = findViewById(R.id.NameET);
-        vendorET = findViewById(R.id.DeptET);
-        deptET = findViewById(R.id.DeptET);
-        isleET = findViewById(R.id.IsleET);
-        priceET = findViewById(R.id.priceET);
+
         //Button crateBTN, modifyBTN, deleteBTN, pSubmitBTN, pCancleBTN, backBTN;
         crateBTN = findViewById(R.id.createBTN);
         modifyBTN = findViewById(R.id.ModifyBTN);
@@ -64,156 +70,63 @@ public class AdminProductScreenActivity extends AppCompatActivity {
         pSubmitBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                vendor = vendorET.getText().toString();
-                price = Double.valueOf(vendorET.getText().toString());
-                dept = deptET.getText().toString();
-                int isleNum = Integer.valueOf(isleET.getText().toString());
+//                vendor = vendorET.getText().toString();
+//                price = Double.valueOf(vendorET.getText().toString());
+//                dept = deptET.getText().toString();
+//                int isleNum = Integer.valueOf(isleET.getText().toString());
                 //STUBBED LOGIC
 
 
-                if(submitCode == -1){
+                if (submitCode == -1) {
                     resultTV.setText("Not able to submit at this time");
-                }
-                else if (submitCode == 1){
-                    if(CommodityMockModelTakeTwo.shared.addCommodity(barcode,name,vendor,dept,isleNum,price)){
+                } else if (submitCode == 1) {
+//                    if(CommodityMockModelTakeTwo.shared.addCommodity(barcode,name,vendor,dept,price)){
+//                        // success
+//                        Toast.makeText(controller, "success", Toast.LENGTH_SHORT).show();
+//                    }
+//                    else{
+//                        // fail
+//                        Toast.makeText(controller, "fail", Toast.LENGTH_SHORT).show();
+//                    }
+
+                } else if (submitCode == 2) {
+//                    if(CommodityMockModelTakeTwo.shared.modifyCommodityFromBarcode(barcode,name,vendor,dept,isleNum,price)){
+//                        // success
+//                        Toast.makeText(controller, "success", Toast.LENGTH_SHORT).show();
+//                    }
+//                    else{
+//                        // fail
+//                        Toast.makeText(controller, "fail", Toast.LENGTH_SHORT).show();
+//                    }
+                } else if (submitCode == 3) {
+                    if (CommodityMockModelTakeTwo.shared.removeItem(barcode)) {
                         // success
                         Toast.makeText(controller, "success", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
+                    } else {
                         // fail
                         Toast.makeText(controller, "fail", Toast.LENGTH_SHORT).show();
                     }
 
                 }
-                else if(submitCode == 2){
-                    if(CommodityMockModelTakeTwo.shared.modifyCommodityFromBarcode(barcode,name,vendor,dept,isleNum,price)){
-                        // success
-                        Toast.makeText(controller, "success", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        // fail
-                        Toast.makeText(controller, "fail", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else if(submitCode == 3){
-                    if(CommodityMockModelTakeTwo.shared.removeItem(barcode)){
-                        // success
-                        Toast.makeText(controller, "success", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        // fail
-                        Toast.makeText(controller, "fail", Toast.LENGTH_SHORT).show();
-                    }
 
-                }
-                crateBTN.setVisibility(View.VISIBLE);
-                modifyBTN.setVisibility(View.VISIBLE);
-                deleteBTN.setVisibility(View.VISIBLE);
             }
 
         });
         pCancleBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hideAndClear();
-                crateBTN.setVisibility(View.VISIBLE);
-                modifyBTN.setVisibility(View.VISIBLE);
-                deleteBTN.setVisibility(View.VISIBLE);
+                FragmentTransaction transaction1 = manager.beginTransaction();
+                transaction1.show(cud);
+                transaction1.hide(input);
+
             }
         });
-        crateBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //So you can't do other things
-                modifyBTN.setVisibility(View.INVISIBLE);
-                deleteBTN.setVisibility(View.INVISIBLE);
-                //Need to make sure the id does not exist
-                hideAndClear();
-                barcode = barCodeET.getText().toString();
-                if(AdminInputValidationHandler.isExistingValue(true, barcode)){
-                    String no = "Product with that Barcode name already exists";
-                    resultTV.setText("" + no);
-                }
-                else{
-                    //Now we are going to show all of the fields
-                    submitCode = 1;
-                    showFields();
-                    Toast.makeText(getApplicationContext(),"Enter commodity details", Toast.LENGTH_LONG).show();
-
-                }
-            }
-        });
-        modifyBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                crateBTN.setVisibility(View.INVISIBLE);
-                deleteBTN.setVisibility(View.INVISIBLE);
-                //Need to make sure the id does not exist
-                hideAndClear();
-                barcode= barCodeET.getText().toString();
-                if(!AdminInputValidationHandler.isExistingValue(true, barcode)){
-                    String no = "Barcode  does not exist";
-                    resultTV.setText("" + no);
-                }
-
-                else{
-                    submitCode = 2;
-                    //Now we are going to show all of the fields
-                    showFields();
-
-                    Commodity subject = CommodityMockModelTakeTwo.shared.getCommodityFromBarcode(barcode);
-
-                    nameET.setText("" + subject.name);
-                    vendorET.setText("" + subject.vendorName);
-                    priceET.setText("" + subject.price);
-                    deptET.setText("" + subject.department);
-                    isleET.setText("" + subject.location);
 
 
-                    Toast.makeText(getApplicationContext(),"Modify Commodity details", Toast.LENGTH_LONG).show();
-
-                }
-            }
-        });
-        deleteBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                crateBTN.setVisibility(View.INVISIBLE);
-                modifyBTN.setVisibility(View.INVISIBLE);
-                //Need to make sure the id does not exist
-                hideAndClear();
-                barcode= barCodeET.getText().toString();
-                if(!AdminInputValidationHandler.isExistingValue(true, barcode)){
-                    String no = "Barcode  does not exist";
-                    resultTV.setText("" + no);
-                }
-
-                else{
-                    submitCode = 3;
-                    //Now we are going to show all of the fields
-                    showFields();
-
-                    Commodity subject = CommodityMockModelTakeTwo.shared.getCommodityFromBarcode(barcode);
-                    nameET.setClickable(false);
-                    vendorET.setClickable(false);
-                    priceET.setClickable(false);
-                    deptET.setClickable(false);
-                    isleET.setClickable(false);
-                    nameET.setText("" + subject.name);
-                    vendorET.setText("" + subject.vendorName);
-                    priceET.setText("" + subject.price);
-                    deptET.setText("" + subject.department);
-                    isleET.setText("" + subject.location);
-                    Toast.makeText(getApplicationContext(),"Confrim Deletion", Toast.LENGTH_LONG).show();
-
-                }
-            }
-        });
 
 
     }
-
-    public void goBackAction(View v){
+    public void goBackAction (View v){
         Intent data = new Intent();
         data.putExtra("EMPID", empid);
         setResult(0, data);
@@ -221,41 +134,41 @@ public class AdminProductScreenActivity extends AppCompatActivity {
     }
 
 
-   private void hideAndClear(){
-        nameTV.setVisibility(View.INVISIBLE);
-        nameET.setVisibility(View.INVISIBLE);
-        nameET.setText("");
-        vendorTV.setVisibility(View.INVISIBLE);
-        vendorET.setVisibility(View.INVISIBLE);
-        vendorET.setText("");
-       deptTV.setVisibility(View.INVISIBLE);
-       deptET.setVisibility(View.INVISIBLE);
-       deptET.setText("");
-       isleTV.setVisibility(View.INVISIBLE);
-       isleET.setVisibility(View.INVISIBLE);
-       isleET.setText("");
-       priceTV.setVisibility(View.INVISIBLE);
-       priceET.setVisibility(View.INVISIBLE);
-       priceET.setText("");
-       nameET.setClickable(true);
-       vendorET.setClickable(true);
-       priceET.setClickable(true);
-       deptET.setClickable(true);
-       isleET.setClickable(true);
+    private void hideAndClear() {
+//        nameTV.setVisibility(View.INVISIBLE);
+//        nameET.setVisibility(View.INVISIBLE);
+//        nameET.setText("");
+//        vendorTV.setVisibility(View.INVISIBLE);
+//        vendorET.setVisibility(View.INVISIBLE);
+//        vendorET.setText("");
+//       deptTV.setVisibility(View.INVISIBLE);
+//       deptET.setVisibility(View.INVISIBLE);
+//       deptET.setText("");
+//       isleTV.setVisibility(View.INVISIBLE);
+//       isleET.setVisibility(View.INVISIBLE);
+//       isleET.setText("");
+//       priceTV.setVisibility(View.INVISIBLE);
+//       priceET.setVisibility(View.INVISIBLE);
+//       priceET.setText("");
+//       nameET.setClickable(true);
+//       vendorET.setClickable(true);
+//       priceET.setClickable(true);
+//       deptET.setClickable(true);
+//       isleET.setClickable(true);
 
 
     }
-    private  void showFields(){
-        nameTV.setVisibility(View.VISIBLE);
-        nameET.setVisibility(View.VISIBLE);
-        vendorTV.setVisibility(View.VISIBLE);
-        vendorET.setVisibility(View.VISIBLE);
-        deptTV.setVisibility(View.VISIBLE);
-        deptET.setVisibility(View.VISIBLE);
-        isleTV.setVisibility(View.VISIBLE);
-        isleET.setVisibility(View.VISIBLE);
-        priceTV.setVisibility(View.VISIBLE);
-        priceET.setVisibility(View.VISIBLE);
+    private void showFields() {
+//        nameTV.setVisibility(View.VISIBLE);
+//        nameET.setVisibility(View.VISIBLE);
+//        vendorTV.setVisibility(View.VISIBLE);
+//        vendorET.setVisibility(View.VISIBLE);
+//        deptTV.setVisibility(View.VISIBLE);
+//        deptET.setVisibility(View.VISIBLE);
+//        isleTV.setVisibility(View.VISIBLE);
+//        isleET.setVisibility(View.VISIBLE);
+//        priceTV.setVisibility(View.VISIBLE);
+//        priceET.setVisibility(View.VISIBLE);
     }
 
 
@@ -263,6 +176,81 @@ public class AdminProductScreenActivity extends AppCompatActivity {
         return (s == null || s.equals("") || s.equals(" "));
     }
 
+    public void buttonPressed(int code, String barcode)
+    {
+        FragmentTransaction transaction = manager.beginTransaction();
+        if (code == 1) {
+            //So you can't do other things
+
+            //Need to make sure the id does not exist
+
+//                barcode = barCodeET.getText().toString();
+            if (AdminInputValidationHandler.isExistingValue(true, barcode)) {
+                String no = "Product with that Barcode name already exists";
+                resultTV.setText("" + no);
+            }
+            else {
+                //Now we are going to show all of the fields
+                transaction.show(input);
+                transaction.hide(cud);
+                submitCode = 1;
+                Toast.makeText(getApplicationContext(), "Enter commodity details", Toast.LENGTH_LONG).show();
+
+            }
+        } else if (code == 2) {
+            if (!AdminInputValidationHandler.isExistingValue(true, barcode)) {
+                String no = "Barcode  does not exist";
+                resultTV.setText("" + no);
+            } else {
+                transaction.show(input);
+                transaction.hide(cud);
+                submitCode = 2;
+                //Now we are going to show all of the fields
 
 
+                Commodity subject = CommodityMockModelTakeTwo.shared.getCommodityFromBarcode(barcode);
+//
+//                    nameET.setText("" + subject.name);
+//                    vendorET.setText("" + subject.vendorName);
+//                    priceET.setText("" + subject.price);
+//                    deptET.setText("" + subject.department);
+//                    isleET.setText("" + subject.location);
+
+
+                Toast.makeText(getApplicationContext(), "Modify Commodity details", Toast.LENGTH_LONG).show();
+
+            }
+        } else if (code == 3) {
+            crateBTN.setVisibility(View.INVISIBLE);
+            modifyBTN.setVisibility(View.INVISIBLE);
+            //Need to make sure the id does not exist
+            hideAndClear();
+//                barcode= barCodeET.getText().toString();
+            if (!AdminInputValidationHandler.isExistingValue(true, barcode)) {
+                String no = "Barcode  does not exist";
+                resultTV.setText("" + no);
+            } else {
+                transaction.show(input);
+                transaction.hide(cud);
+                submitCode = 3;
+
+
+             //   Commodity subject = CommodityMockModelTakeTwo.shared.getCommodityFromBarcode(barcode);
+//                    nameET.setClickable(false);
+//                    vendorET.setClickable(false);
+//                    priceET.setClickable(false);
+//                    deptET.setClickable(false);
+//                    isleET.setClickable(false);
+//                    nameET.setText("" + subject.name);
+//                    vendorET.setText("" + subject.vendorName);
+//                    priceET.setText("" + subject.price);
+//                    deptET.setText("" + subject.department);
+//                    isleET.setText("" + subject.location);
+                Toast.makeText(getApplicationContext(), "Confrim Deletion", Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+        transaction.commit();
+    }
 }
--- SmartShopper/app/src/main/java/com/example/smartshopper/ProductCUD.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/ProductCUD.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -24,6 +26,12 @@ public class ProductCUD extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    Button createBTN, modifyBTN, destroyBTN;
+    EditText barcodeET;
+    public interface CUDopperations{
+        public void buttonPressed(int modCode, String barcode);
+    }
+    private CUDopperations myActivity = null;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -66,7 +74,34 @@ public class ProductCUD extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_cud, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_product_cud, container, false);
+        barcodeET = v.findViewById(R.id.BarcodeET);
+        createBTN = v.findViewById(R.id.createBTN);
+        createBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myActivity.buttonPressed(1, barcodeET.getText().toString());
+            }
+        });
+        modifyBTN = v.findViewById(R.id.ModifyBTN);
+        modifyBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myActivity.buttonPressed(2, barcodeET.getText().toString());
+
+            }
+        });
+        destroyBTN = v.findViewById(R.id.RemoveBTN);
+        destroyBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myActivity.buttonPressed(3, barcodeET.getText().toString());
+
+            }
+        });
+
+        return  v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
