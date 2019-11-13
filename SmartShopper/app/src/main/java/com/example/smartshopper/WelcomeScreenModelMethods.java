a5e95ce92e99db27ee7e83a853171f63fd783792
Matthew Berry <41410700+Infraction71@users.noreply.github.com>
2019-11-12 20:16:31 -0600

    Create WelcomeScreenModelMethods.java
    Jared use this to create the two callback mehtods. Use findAllStores() and populateDeapartmentListForStore

---
+++ SmartShopper/app/src/main/java/com/example/smartshopper/WelcomeScreenModelMethods.java
@@ -0,0 +1,7 @@
+package com.example.smartshopper;
+
+public interface WelcomeScreenModelMethods {
+    //This method will be used for creation
+    public void storeCB(Store store );
+    public void departmentCB(Store store);
+}
