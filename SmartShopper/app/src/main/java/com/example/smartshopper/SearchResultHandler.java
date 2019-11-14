49d796dd6105092cb8d060f2bd66441b85d62d55
corder1999 <42747372+corder13@users.noreply.github.com>
2019-11-14 10:48:15 -0600

    Created the SearchResultHandler

---
+++ SmartShopper/app/src/main/java/com/example/smartshopper/SearchResultHandler.java
@@ -0,0 +1,8 @@
+package com.example.smartshopper;
+
+public class SearchResultHandler{
+
+    interface Interface{
+        public void SearchResultHandler(boolean operationSuccess, boolean searchGood, Commodity listCommodities);
+    }
+}
