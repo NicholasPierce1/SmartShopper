d52ca303306f0762993d46d5c46189b5ced1c1ec
corder1999 <42747372+corder13@users.noreply.github.com>
2019-11-14 19:43:41 -0600

    working on methods

--- SmartShopper/app/src/main/java/com/example/smartshopper/Welcome_screenActivity.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/Welcome_screenActivity.java
@@ -1,6 +1,11 @@
 package com.example.smartshopper;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +13,12 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Welcome_screenActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, WelcomeScreenModelMethods{
-
 
     // list of store objects from the model/broker
     private List<Store> storeList = null;
@@ -31,16 +37,16 @@ public class Welcome_screenActivity extends AppCompatActivity implements Adapter
         setContentView(R.layout.welcome_screen);
         spin = findViewById(R.id.storeMenuSPNR);
         spin.setOnItemSelectedListener(this);
-//        ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, convertFromStoreArray(stores));
-//        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spin.setAdapter(aa);
+
+        // important: enjoin broker to get stores and init data persistence w/ app context
+        Model.getShared().findAllStores(super.getApplicationContext(), this);
     }
 
 
     //Performing action onItemSelected and onNothing selected
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
-        //TODO: update local ref to user selection
+        this.currentStore = this.storeList.get(position);
         
     }
 
@@ -68,13 +74,92 @@ public class Welcome_screenActivity extends AppCompatActivity implements Adapter
     }
 
 
+    // if success is true-- create array adapter, set array adapter to spinner & set list to 'this.storeList'
+    // else -- create alert dialog informing user error occured, ask user if they want a re-try
     @Override
-    public void storeCB(boolean success, List<Store> store) {
+    public void storeCB(boolean success, List<Store> stores) {
+        if(success = true){
+            this.storeList = stores;
+            // invokes helper to convert list of stores to list of strings
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, this.convertStoreListToStringList() );
+            arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spin.setAdapter(arrayAdapter);
+        }
+        else{
+            // local ref to this
+            final Welcome_screenActivity welcome_screenActivity = this;
 
+            // create local Alert Dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            // sets title
+            builder.setTitle("Oops...");
+
+            // sets message of error
+            builder.setMessage("Looks like an error occurred getting your stores! Please try again or come back later!");
+
+            // sets only, positive message
+            builder.setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    // invoke operation again
+                    Model.getShared().findAllStores(getApplicationContext(), welcome_screenActivity);
+                }
+            });
+
+            // shows dialog
+            builder.create().show();
+        }
     }
 
+    // if success is true- get intent that was set and start it, else alert dialog to inform user and ask if they want a re-try
     @Override
     public void departmentCB(boolean success) {
+        /*
+        // acquires current store and passes into init depts
+        Model.getShared().initializeDeapartmentListForStore(this.currentStore, this);
+         */
+        if(success = true){
 
+        }
+        else{
+            // local ref to this
+            final Welcome_screenActivity welcome_screenActivity = this;
+
+            // create local Alert Dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            // sets title
+            builder.setTitle("Oops...");
+
+            // sets message of error
+            builder.setMessage("Looks like an error occurred getting your stores! Please try again or come back later!");
+
+            // sets only, positive message
+            builder.setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    // invoke operation again
+                    Model.getShared().findAllStores(getApplicationContext(), welcome_screenActivity);
+                }
+            });
+
+            // shows dialog
+            builder.create().show();
+        }
+    }
+
+    // helper to convert list of stores to list of friendly strings (store names)
+    @NonNull
+    private List<String> convertStoreListToStringList(){
+
+        final List<String> listOfStoreNames = new ArrayList<String>();
+
+        for(Store store: this.storeList)
+            listOfStoreNames.add(store.name);
+
+        return listOfStoreNames;
     }
 }
