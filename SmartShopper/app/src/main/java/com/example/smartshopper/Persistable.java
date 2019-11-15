46f46f953e3980a10db83e24fc645f6a5c8eb6cc
corder1999 <42747372+corder13@users.noreply.github.com>
2019-11-14 18:06:20 -0600

    start creating search

--- SmartShopper/app/src/main/java/com/example/smartshopper/Persistable.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/Persistable.java
@@ -7,7 +7,7 @@ import com.parse.ParseObject;
 
 // supplements the implementation/behavior of converting a read-- acquired from Back4App to a composite DA-- relational data not attached
 // also appends functionality to convert a composite OR complete to a parsed object for persistence
-public interface Persistable {
+public interface Persistable{
 
     @NonNull
     public abstract ParseObject toParseObject();
--- SmartShopper/app/src/main/java/com/example/smartshopper/SearchResultHandler.java
+++ SmartShopper/app/src/main/java/com/example/smartshopper/SearchResultHandler.java
@@ -5,5 +5,6 @@ import android.widget.LinearLayout;
 import java.util.List;
 
 public interface SearchResultHandler extends CallBackInterface{
+
     public void searchCB(boolean operationSuccess, boolean searchGood, List<Commodity> commodityList);
 }
